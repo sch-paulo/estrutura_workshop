0e170d1ba5fdca33e343bf49216f833dd4cc0f07
sch-paulo <pschmitzjr@gmail.com>
2025-04-23 19:24:39 -0400

    extract working

---
+++ app/main.py
@@ -0,0 +1,5 @@
+from pipeline.extract import extract_from_excel
+
+listas_de_dataframe = extract_from_excel('data/raw')
+
+print(listas_de_dataframe)
---
+++ app/pipeline/extract.py
@@ -0,0 +1,27 @@
+import os
+import glob
+import pandas as pd
+from typing import List
+
+PATH = 'data/raw'
+
+def extract_from_excel(path: str) -> List[pd.DataFrame]:
+    '''
+    Função para ler os arquivos de uma pasta 'data/raw' e retornar uma lista de dataframes
+
+    args: input_path (str): caminho da pasta com os arquivos
+
+    return: lista de dataframes
+    '''
+
+    all_files = glob.glob(os.path.join(path, '*.xlsx'))
+
+    dataframe_list = []
+    for file in all_files:
+        dataframe_list.append(pd.read_excel(file))
+
+    return dataframe_list
+
+if __name__ == '__main__':
+    dataframe_list = extract_from_excel(PATH)
+    print(dataframe_list)
