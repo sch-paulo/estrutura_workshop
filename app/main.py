dab302b6061c10e2d567ea7ac07d364cfaf31623
sch-paulo <pschmitzjr@gmail.com>
2025-04-23 20:34:40 -0400

    load and pipeline working

--- app/main.py
+++ app/main.py
@@ -1,5 +1,11 @@
 from pipeline.extract import extract_from_excel
+from pipeline.transform import concat_dataframes
+from pipeline.load import load_excel
 
-listas_de_dataframe = extract_from_excel('data/raw')
+if __name__ == '__main__':
+    dataframe_list = extract_from_excel('data/raw')
+    print(type(dataframe_list))
+    df_concat = concat_dataframes(dataframe_list)
+    print(type(df_concat))
+    load_excel(df_concat, 'data/processed', 'output')
 
-print(listas_de_dataframe)
